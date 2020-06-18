63236bcd35078213985244da08b392a358088833
Jack.Kauphusman <jack.kauphusman@outlook.com>
2020-06-18 10:22:57 -0500

    Preliminary R Script for the inaturalist personal website

--- InaturalistProj-Script.R
+++ InaturalistProj-Script.R
@@ -1,1 +1,22 @@
 #Begining of script to push to project test
+library(rinat)
+library(leaflet)
+library(dplyr)
+library(sp)
+library(plotly)
+
+my_obs<-get_inat_obs_user(username = "jackkay", maxresults = 1000)
+
+str(my_obs)
+#Color iconic taxon name
+pal <- colorFactor(
+  palette = c("purple", "blue", "yellow", "red", "sandybrown", "green", "black"),
+  domain = my_obs$iconic_taxon_name
+)
+
+
+leaflet(my_obs)%>% 
+  addTiles() %>%
+  addCircleMarkers(~longitude,~latitude, weight = 5,
+             popup = ~as.character(common_name), color = ~pal(iconic_taxon_name))
+  
